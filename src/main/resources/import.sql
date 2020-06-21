2a29af55efe59f30f5c15d3f7e9d281ae98c9145
Daniele D'Agostino <principelele@gmail.com>
2020-06-21 19:39:12 +0200

    Insert for new movie Indiana Jones added

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,7 +3,7 @@ insert into director(id, name) values(2, 'Martin Scorzese');
 insert into director(id, name) values(3, 'Quentin Tarantino');
 
 insert into movie(id, name, director_id) values(1, 'Duel', 1);
-insert into movie(id, name, director_id) values(2, 'The Shark', 1);
+insert into movie(id, name, director_id) values(2, 'Jaws', 1);
 insert into movie(id, name, director_id) values(3, 'Close Encounters of the Third Kind', 1);
 insert into movie(id, name, director_id) values(4, 'Reservoir Dogs', 3);
 insert into movie(id, name, director_id) values(5, 'Pulp Fiction', 3);
@@ -11,6 +11,7 @@ insert into movie(id, name, director_id) values(6, 'Kill Bill: Volume 1', 3);
 insert into movie(id, name, director_id) values(7, 'Taxi Driver', 2);
 insert into movie(id, name, director_id) values(8, 'New York, New York', 2);
 insert into movie(id, name, director_id) values(9, 'Raging Bull', 2);
+insert into movie(id, name, director_id) values(10, 'Indiana Jones and the Last Crusade', 1);
 
 insert into rating(id, score, insert_date, movie_id) values(1, 2.0, current_timestamp(), 1);
 insert into rating(id, score, insert_date, movie_id) values(2, 5.5, current_timestamp(), 1);
