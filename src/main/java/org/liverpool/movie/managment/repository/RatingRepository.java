c744bccc687a17aac25a767084740738681b8f45
Daniele D'Agostino <principelele@gmail.com>
2020-06-22 18:24:40 +0200

    Movie projection altered

--- src/main/java/org/liverpool/movie/managment/repository/RatingRepository.java
+++ src/main/java/org/liverpool/movie/managment/repository/RatingRepository.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.liverpool.movie.managment.model.Rating;
-import org.liverpool.movie.managment.projection.RatingProjection;
+import org.liverpool.movie.managment.projection.MovieProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -19,7 +19,7 @@ public interface RatingRepository extends JpaRepository<Rating, Integer> {
 	 * @param {@link BigDecimal} score
 	 * @return a {@link List} of Rating with only its identifier
 	 */
-	@Query(value = "select movie.id, max(insertDate) from Rating where score > :score "
-			+ "group by movie.id")
-	List<RatingProjection> searchMoviesWithScoreAbove(@Param("score") BigDecimal score);
+	@Query(value = "select movie.id, movie.name, max(insertDate) from Rating where score > :score "
+			+ "group by movie.id, movie.name")
+	List<MovieProjection> searchMoviesWithScoreAbove(@Param("score") BigDecimal score);
 }
